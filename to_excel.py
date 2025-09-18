f4f7bdaa3bee4dcbe0d2539830c5ae86979a56c7
Melody <momelody33@gmail.com>
2025-09-18 14:03:09 -0400

    Convert the lox sites to excel

--- to_excel.py
+++ to_excel.py
@@ -23,12 +23,11 @@ def convert_csv_to_excel(csv_file, excel_file):
         print(f"Error writing Excel file: {e}")
 
 if __name__ == "__main__":
-    csv_input_file = "recombinase_total_contacts.csv"
-    excel_output_file = "recombinase_total_contacts.xlsx"
-    convert_csv_to_excel(csv_input_file, excel_output_file)
-    csv_input_file = "recombinase_unique_contacts.csv"
-    excel_output_file = "recombinase_unique_contacts.xlsx"
-    convert_csv_to_excel(csv_input_file, excel_output_file)
-    csv_input_file = "recombinase_summary.csv"
-    excel_output_file = "recombinase_summary.xlsx"
-    convert_csv_to_excel(csv_input_file, excel_output_file)
+
+    for csv_in, csv_out in [
+        ("lox_site_results.csv", "lox_site_results.xlsx"),
+        ("recombinase_unique_contacts.csv", "recombinase_unique_contacts.xlsx"),
+        ("recombinase_total_contacts.csv", "recombinase_total_contacts.xlsx"),
+        ("recombinase_summary.csv", "recombinase_summary.xlsx")
+    ]:
+        convert_csv_to_excel(csv_in, csv_out)
